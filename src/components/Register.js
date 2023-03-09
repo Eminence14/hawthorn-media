5d4b22c894d4036e2f44bb286a005fba9087e4d8
V <victoribidunmoye@gmail.com>
2023-03-09 19:24:32 +0100

    add login and register desktop viewport

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,7 @@
 import { useLayoutEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../assets/images/Hawthorn Studios Yellow.png';
+import hawthorn from '../assets/images/our_services1.jpg'
 
 const Register = () => {
     useLayoutEffect(() => {
@@ -25,46 +26,49 @@ const Register = () => {
 
     return (
         <div className="auth_container">
-            <header>
-                <img src={Logo} alt="logo" />
-            </header>
-            <main>
-                <section className="welcome">
-                    <span>Welcome</span>
-                    <span>Let’s get you signed up.</span>
-                </section>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="email">
-                        Email
-                    </label>
-                    <input type="email" id="email" name='email' placeholder='Enter your email' autoComplete='off' />
-
-                    <div className="pass">
-                        <label htmlFor="password">
-                            Password
+            <div className="auth-left">
+                <header>
+                    <img src={Logo} alt="logo" />
+                </header>
+                <main>
+                    <section className="welcome">
+                        <span>Welcome</span>
+                        <span>Let’s get you signed up.</span>
+                    </section>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="email">
+                            Email
                         </label>
-                        <span className="open_pass" onClick={() => { clickHandler(togglePass) }}></span>
-                        <input type="password" id="password" name='password' placeholder='Enter your password' autoComplete='off'
-                            ref={togglePass}
-                        />
-                    </div>
+                        <input type="email" id="email" name='email' placeholder='Enter your email' autoComplete='off' />
 
-                    <div className="pass">
-                        <label htmlFor="cnPass">
-                            Confirm Password
-                        </label>
-                        <span className="open_pass" onClick={() => { clickHandler(togglePass2) }}></span>
-                        <input type="password" id="cnPass"
-                            name='cnPass' placeholder='Enter your password' autoComplete='off'
-                            ref={togglePass2}
-                        />
-                    </div>
+                        <div className="pass">
+                            <label htmlFor="password">
+                                Password
+                            </label>
+                            <span className="open_pass" onClick={() => { clickHandler(togglePass) }}></span>
+                            <input type="password" id="password" name='password' placeholder='Enter your password' autoComplete='off'
+                                ref={togglePass}
+                            />
+                        </div>
+
+                        <div className="pass">
+                            <label htmlFor="cnPass">
+                                Confirm Password
+                            </label>
+                            <span className="open_pass" onClick={() => { clickHandler(togglePass2) }}></span>
+                            <input type="password" id="cnPass"
+                                name='cnPass' placeholder='Enter your password' autoComplete='off'
+                                ref={togglePass2}
+                            />
+                        </div>
 
 
-                    <button>Sign up</button>
-                </form>
-            </main>
-            <Link to='/login'> Already have an account? <span> Sign in </span> </Link>
+                        <button className='trans-right'>Sign up</button>
+                    </form>
+                </main>
+                <Link to='/login' className='trans-right'> Already have an account? <span> Sign in </span> </Link>
+            </div>
+            <div className="auth-right"></div>
         </div>
     );
 }
